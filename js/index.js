1c744504cb643338d08818557678d28e750b79c7
Nilsonlinux <nilsonlinux@gmail.com>
2018-11-13 22:51:10 -0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -14,18 +14,18 @@
   }
   
   data = [
-    "Hello!",
-    "I'm Joshua Kunst",
-    "I'm a #bug #coder",
-    "I'm a data lover",
-    "I'm a data wrangler",
-    "I'm a data explorer",
-    "I'm a data asdf... :B",
-    "I <3 my familiy",
-    "I <3 code",
-    "I <3 visualizations",
-    "I <3 R",
-    "Thanks for your visit ;)!",
+    "Olá!",
+    "Eu sou Nilsonlinux",
+    "Funciońário do Mateus Supermercados",
+    "Na área de sistemas",
+    "Usuário de Linux desde 2006",
+    "Estudante de Jekyll",
+    "HTML, PHP...",
+    "Apreciador de Markdown",
+    "Software",
+    "Interface.",
+    "Obrigado por visitar",
+    "Agradeço... ;)!",
     ""
     ];
   
